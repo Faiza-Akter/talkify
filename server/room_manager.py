5c5c749c2dc3428fd18c67c4487e531150fbab6e
Faiza Akter <faizaakter2004@gmail.com>
2026-04-30 18:09:12 +0600

    Update room manager logic

--- server/room_manager.py
+++ server/room_manager.py
@@ -6,20 +6,38 @@ from shared.config import DEFAULT_ROOM
 class RoomManager:
     def __init__(self):
         """
-        Stores room memberships:
+        Stores room memberships and optional passwords:
         {
             "General": {"Faiza", "Ishfak"},
             "Study": {"Faiza"}
         }
         """
-        self.rooms = {
-            DEFAULT_ROOM: set()
-        }
+        self.rooms = {DEFAULT_ROOM: set()}
+        self.room_passwords = {DEFAULT_ROOM: ""}
+        self.room_creators = {}
 
-    def create_room_if_not_exists(self, room_name):
+    def create_room_if_not_exists(self, room_name, password="", creator=None):
         """Create room if it does not already exist."""
         if room_name not in self.rooms:
             self.rooms[room_name] = set()
+            self.room_passwords[room_name] = password or ""
+            if creator:
+                self.room_creators[room_name] = creator
+
+    def create_room(self, room_name, creator, password="", members=None):
+        self.create_room_if_not_exists(room_name, password=password, creator=creator)
+        self.rooms[room_name].add(creator)
+        for member in members or []:
+            if member:
+                self.rooms[room_name].add(member)
+
+    def can_join_room(self, room_name, password=""):
+        if room_name not in self.rooms:
+            return True
+        expected = self.room_passwords.get(room_name, "")
+        if not expected:
+            return True
+        return expected == (password or "")
 
     def add_user_to_room(self, username, room_name):
         """Add user to a room."""
@@ -43,6 +61,8 @@ class RoomManager:
 
         for room in empty_rooms:
             del self.rooms[room]
+            self.room_passwords.pop(room, None)
+            self.room_creators.pop(room, None)
 
     def get_users_in_room(self, room_name):
         """Return all users in a room."""
